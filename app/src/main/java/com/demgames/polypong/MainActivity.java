8817ffacbdd63be853ea4cc62d83bce522235e51
raphi2 <36761060+raphi2@users.noreply.github.com>
2018-02-26 13:02:31 +0100

    Always On Bildschirm hinzugefüt - Bildschirm schaltet sich nicht mehr aus.

--- app/src/main/java/com/demgames/polypong/MainActivity.java
+++ app/src/main/java/com/demgames/polypong/MainActivity.java
@@ -1,10 +1,12 @@
 package com.demgames.polypong;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.net.wifi.WifiManager;
 import android.content.Intent;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +19,17 @@ import java.nio.ByteOrder;
 import java.math.BigInteger;
 import java.net.UnknownHostException;
 
+import android.os.PowerManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Always On
+    protected PowerManager.WakeLock mWakeLock;
+    @Override
+    public void onDestroy() {
+        this.mWakeLock.release();
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
         //ToDo Splash screen mit Pixellook integrieren?
         //ToDo Aufteilung des Menüs, Button Optik
         //ToDo eventuell nur Portrait modus für menu einstellen
+
+
+         /* will make the screen be always on until this Activity gets destroyed. */
+        final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        this.mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "My Tag");
+        this.mWakeLock.acquire();
 
         /*requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
