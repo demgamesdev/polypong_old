4ac559c849d3388cf289ebdc13b1cb1ce4c8e88b
raphi2 <36761060+raphi2@users.noreply.github.com>
2018-02-26 15:36:28 +0100

    IP Adressen können jetz mit addipToList in die Listview bei client und server hinzugefügt werden.

--- app/src/main/java/com/demgames/polypong/Client.java
+++ app/src/main/java/com/demgames/polypong/Client.java
@@ -6,20 +6,26 @@ import android.os.Bundle;
 import android.net.wifi.WifiManager;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.net.InetAddress;
 import java.nio.ByteOrder;
 import java.math.BigInteger;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.w3c.dom.Text;
 
 public class Client extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,18 @@ public class Client extends AppCompatActivity {
 
         final TextView myIpTextView = (TextView) findViewById(R.id.IpAdressTextView);
         final Button devBtn = (Button) findViewById(R.id.devButton);
+        final ListView ClientLV = (ListView) findViewById(R.id.ClientListView);
+
+        //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+        String[] IPAdressen = new String[] {        };
+
+        final List<String> server_list = new ArrayList<String>(Arrays.asList(IPAdressen));
+
+        //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, server_list);
+        ClientLV.setAdapter(arrayAdapter);
+
 
         //automatically detect ip if available, create new thread for searching ip
         final Byte testByte=0;
@@ -46,6 +64,8 @@ public class Client extends AppCompatActivity {
                         public void run() {
                             if(myIpAdress!=null) {
                                 myIpTextView.setText("Deine IP-Adresse lautet: " + myIpAdress);
+                                //IP Adresse wird in die Liste Hinzugefügt
+                                addipTolist(myIpAdress, server_list, arrayAdapter);
 
                             } else {
                                 myIpTextView.setText("Unable to get Ip-Adress");
@@ -88,8 +108,6 @@ public class Client extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
-
-
     protected String wifiIpAddress(Context context) {
         WifiManager wifiManager = (WifiManager) context.getSystemService(WIFI_SERVICE);
         int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
@@ -111,7 +129,6 @@ public class Client extends AppCompatActivity {
 
         return ipAddressString;
     }
-
     boolean checkIfIp(String teststring) {
         String[] parts=teststring.split("\\.");
         if(parts.length==4) {
@@ -121,5 +138,12 @@ public class Client extends AppCompatActivity {
         return (false);
 
     }
-
+    //IP in die Liste hinzufügen
+    public void addipTolist(String IP, List server_list, ArrayAdapter arrayAdapter){
+        if(!server_list.contains(IP)){
+            server_list.add(IP);
+            //Liste wird aktualisiert
+            arrayAdapter.notifyDataSetChanged();
+        }
+    }
 }
