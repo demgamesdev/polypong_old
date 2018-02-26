12849567e8b836e7aca0d1f972e351b2df91217e
raphi2 <36761060+raphi2@users.noreply.github.com>
2018-02-26 13:02:31 +0100

    Always On Bildschirm hinzugefüt - Bildschirm schaltet sich nicht mehr aus.

--- app/src/main/java/com/demgames/polypong/Options.java
+++ app/src/main/java/com/demgames/polypong/Options.java
@@ -1,7 +1,9 @@
 package com.demgames.polypong;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
